f9fb1add594a559cbf73bc78f8fa0472f4694c17
Judson Ciribelli <judson.ciribelli@discente.ufma.br>
2025-03-10 15:36:22 -0300

    update api

--- src/app/utils/api.js
+++ src/app/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API_URL =
-  "https://dfd0-2804-d4b-7766-ad00-51dd-aa76-2bba-cb5b.ngrok-free.app/api/";
+  "https://61fd-2804-1b2-ab42-fcdb-3df1-c057-a5ae-645b.ngrok-free.app/api/";
 
 const api = axios.create({
   baseURL: API_URL,
